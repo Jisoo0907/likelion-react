23a54eefd7630972bf20718174bc01548bd75971
jisoolee <szjslee@gmail.com>
2024-07-26 02:43:24 +0900

    exercise JavaScript - optional chaining

--- 03-ecmascript-for-react/src/01-ecmascript/03-nullish-optional-chaining.js
+++ 03-ecmascript-for-react/src/01-ecmascript/03-nullish-optional-chaining.js
@@ -5,25 +5,31 @@
 // --------------------------------------------------------------------------
 
 function nullish() {
-  let value = 0;
+  let value = 0; // 개발자의 의도!!!!
 
-  let result = value || 100;
-  console.log({ '||': result });
+  let result = value || 100; // 첫 번째 truthy 값 반환
+  console.log({ "||": result });
 
+  // nullish: Web Native(추가적인 라이브러리나 폴리필 없이 브라우저에서 바로 사용할 수 있는 기능)
   function isNullOrUndefined(value) {
     return value === null || value === undefined ? true : false;
+    // 지금 value는 null도 undefinedㄷ 아니니까 false 반환
   }
-
+  // isNullOrUndefined가 false를 반환했으니 !false는 true
+  // 따라서 ? 다음에 오는 첫 번째 값 선택함
   result = !isNullOrUndefined(value) ? value : 100;
   console.log({ isNullOrUndefined: result });
 
   // 🔶 null 병합 연산자 코드를 작성합니다.
+  // 왼쪽 피연산자가 null || undefined => 오른쪽 피연산자 반환, or 왼쪽 피연산자 반환
+  result = value ?? 100;
+  console.log({ "??": result });
   // 참고: https://mzl.la/3vQUYin | https://mzl.la/3PXiOQ9
 }
 
 function optionalChaining() {
   const topic = {
-    _title: '매년 업데이트 되는 ECMAScript',
+    _title: "매년 업데이트 되는 ECMAScript",
     getTitle() {
       return this._title;
     },
@@ -32,14 +38,14 @@ function optionalChaining() {
     },
   };
 
-  if (topic && typeof topic === 'object' && !Array.isArray(topic)) {
+  if (topic && typeof topic === "object" && !Array.isArray(topic)) {
     let title, name;
 
-    if (typeof topic.getTitle === 'function') {
+    if (typeof topic.getTitle === "function") {
       title = topic.getTitle();
     }
 
-    if (typeof topic.getName === 'function') {
+    if (typeof topic.getName === "function") {
       name = topic.getName();
     }
 
