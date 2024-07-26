3b03c2cc013aa3f6dd630cca319547225290c0bb
jisoolee <szjslee@gmail.com>
2024-07-26 14:36:23 +0900

    assets - Avatar

--- 05-react-component-with-props/src/main.js
+++ 05-react-component-with-props/src/main.js
@@ -1,6 +1,7 @@
 import React from "https://esm.sh/react";
 import { createRoot } from "https://esm.sh/react-dom";
 import { ArchitectureListPage } from "./pages/ArchitectureListPage.js";
+import { AvatarListPage } from "./pages/AvatarListPage.js";
 
 // 리액트 앱을 렌더링 할 DOM 요소 참조
 const container = document.getElementById("react-app");
@@ -15,3 +16,9 @@ else {
   // 개발자에게 경고
   console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
 }
+
+/* 
+1. 페이지 컴포넌트 작성 및 main.js 연결
+2. Avatar 컴포넌트 작성 및 status 속성(prop) 설정
+3. status 속성(prop)에 따라 화면에 표시되는 Avatar 구성
+*/
---
+++ 05-react-component-with-props/src/pages/AvatarListPage.js
@@ -0,0 +1,7 @@
+import React from "https://esm.sh/react";
+
+function AvatarListPage() {
+  return React.createElement();
+}
+
+export default AvatarListPage;
