3200ca64b5340845691de6e034f70c8514800355
jisoolee <szjslee@gmail.com>
2024-07-29 14:37:40 +0900

    learn React (w. Vite Custom) - ESLint plugin 구성  - react  - react-hooks  - react-refresh

--- 07-vite-custom-template/vite-custom-template/src/main.jsx
+++ 07-vite-custom-template/vite-custom-template/src/main.jsx
@@ -1,13 +1,8 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React 웹 앱</h1>
-    </div>
-  );
-}
+// console.log(import.meta.env);
 
 const domNode = document.getElementById("react-app");
 createRoot(domNode).render(
