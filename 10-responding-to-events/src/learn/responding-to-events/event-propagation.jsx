38e8b8010ae37d1f791add593cadbc118bdedb05
jisoolee <szjslee@gmail.com>
2024-08-05 22:10:05 +0900

    learn React stop propagation 주석 추가

--- 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
+++ 10-responding-to-events/src/learn/responding-to-events/event-propagation.jsx
@@ -2,7 +2,7 @@
 // ✅ 이벤트 전파
 // --------------------------------------------------------------------------
 // - [x] 전파 중지 (개별적으로 할 수 있음)
-// - [x] 전파 대안으로 핸들러 전달
+// - [x] 전파 대안으로 핸들러 전달 => 필요한 핸들러를 직접 자식 요소에 전달하여 이벤트 처리
 // - [x] 기본 작동 방지
 // --------------------------------------------------------------------------
 
@@ -37,11 +37,11 @@ function EventPropagation() {
       <LayoutBox style={styles.cyan} onClick={returnHandlePrint('cyan')}>
         <LayoutBox
           style={styles.magenta}
-          onClick={returnHandlePrint('magenta')}
+          onClick={returnHandlePrint('magenta')} // 각각의 이벤트 핸들러를 직접 받음
         >
           <LayoutBox
             style={styles.yellow}
-            onClick={returnHandlePrint('yellow')}
+            onClick={returnHandlePrint('yellow')} // 필요한 핸들러를 각 요소에 직접 전달함으로써 이벤트 처리
           >
             <LayoutBox
               style={styles.purple}
