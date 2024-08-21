aa1eea188074ccf414c392d714ed968d8f062b9a
jisoolee <szjslee@gmail.com>
2024-08-21 11:02:27 +0900

    4 - 불필요한 렌더링 확인

--- 17-advanced-state-management/src/routes/optimizing-renders/components/TimeAndCounter/index.jsx
+++ 17-advanced-state-management/src/routes/optimizing-renders/components/TimeAndCounter/index.jsx
@@ -2,9 +2,14 @@ import useClock from '@/hooks/useClock';
 import S from './style.module.css';
 import Counter from '../Counter';
 import TimeToggler from './TimeToggler';
+import useRenderCountLog from '@/hooks/useRenderCountLog';
 
 function TimeAndCounter() {
+  // 컴포넌트가 다시 렌더링(실행) 되는 이유
+  // 1. 컴포넌트 자신이 소유한 상태가 변경될 때
+  // 2. 컴포넌트 외부에서 전달된 속성(props)이 변경될 때
   const { time, turnOn, onOff } = useClock();
+  useRenderCountLog('TimeAndCounter', '#062570', 800, 20);
 
   const handleToggleTime = () => onOff((c) => !c);
 
