2c57ab29d242ffd46b22349c3838086767e13ac6
jisoolee <szjslee@gmail.com>
2024-08-21 11:28:21 +0900

    6 - memo 사용

--- 17-advanced-state-management/src/routes/optimizing-renders/components/TimeAndCounter/index.jsx
+++ 17-advanced-state-management/src/routes/optimizing-renders/components/TimeAndCounter/index.jsx
@@ -3,7 +3,11 @@ import S from './style.module.css';
 import Counter from '../Counter';
 import TimeToggler from './TimeToggler';
 import useRenderCountLog from '@/hooks/useRenderCountLog';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
+
+// TimeToggler 함수 참조
+// 기억된 컴포넌트 = React.memo(컴포넌트 참조)
+const MemoizedTimeToggler = React.memo(TimeToggler);
 
 function TimeAndCounter() {
   // 컴포넌트가 다시 렌더링(실행) 되는 이유
@@ -20,7 +24,16 @@ function TimeAndCounter() {
     <div className={S.component}>
       <div role="group" className={S.group}>
         <time>{time}</time>
-        <TimeToggler onToggle={handleToggleTime}>{label}</TimeToggler>
+        {/* <TimeToggler onToggle={handleToggleTime}>{label}</TimeToggler> */}
+        {React.createElement(
+          // 이 안에서 함수 참조는 몇 개?
+          // 함수 참조: 함수를 기억하고 있는 것
+          // 1. handleToggleTime => 얘는 우리가 기억(메모리)함 2. TimeToggler(컴포넌트니까) => 근데 얘는 우리가 기억 안 함.
+          // 그래서 리액트가 렌더링 전과 후가 다르다고 봄 => 리렌더링
+          MemoizedTimeToggler,
+          { onToggle: handleToggleTime },
+          label
+        )}
       </div>
       <Counter />
     </div>
